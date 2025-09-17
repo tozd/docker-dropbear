255ee24b4b97923c4a14582904539772d34d29a0
Mitar <mitar.git@tnode.com>
2025-09-17 09:54:12 +0200

    Fix.

--- test.sh
+++ test.sh
@@ -18,9 +18,10 @@ apk add --no-cache dropbear dropbear-dbclient
 mkdir .ssh
 dropbearkey -t ed25519 -f .ssh/id_dropbear
 dropbearkey -y -f .ssh/id_dropbear | grep "^ssh-ed25519 " > .ssh/authorized_keys
+echo "/sbin/nologin" > shells
 
 echo "Running Docker image"
-docker run -d --name test -p 22:22 -v "$(pwd)/.ssh:/.ssh" "${CI_REGISTRY_IMAGE}:${TAG}"
+docker run -d --name test -p 22:22 -v "$(pwd)/.ssh:/.ssh" -v "$(pwd)/shells:/etc/shells" "${CI_REGISTRY_IMAGE}:${TAG}"
 trap cleanup EXIT
 
 echo "Sleeping"
